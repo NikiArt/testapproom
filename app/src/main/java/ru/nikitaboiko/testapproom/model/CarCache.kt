37de6b6bb25a4d2e3265ca0ecdf357789838c862
Nikita Boiko <genertyniki@gmail.com>
2019-08-13 16:31:03 +0300

    all tasks done

--- app/src/main/java/ru/nikitaboiko/testapproom/model/CarCache.kt
+++ app/src/main/java/ru/nikitaboiko/testapproom/model/CarCache.kt
@@ -22,7 +22,7 @@ class CarCache(private val database: Database) {
         return Single.create {
             val cars = arrayListOf(
                 Car("a123aa777", "Nissan", "X-Trail", 8, "Gold"),
-                Car("a222aa78", "Citroen", "c4", 2, "Black"),
+                Car("a222aa78", "Citroen", "C4", 2, "Black"),
                 Car("a333aa777", "Audi", "Q7", 2, "White"),
                 Car("c321ca777", "Audi", "A3", 12, "White"),
                 Car("a124cc78", "BMW", "X5", 7, "Blue")
